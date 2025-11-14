b59243ab107ea6b9bfff14374b0c8bcaac84ab8c
p-disha <disha.papneja@gmail.com>
2025-11-14 19:02:26 +0530

    Optimizing for render deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const API = "https://rbac-backend.onrender.com/api";
+const API = "https://todolistapi-fc8p.onrender.com";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
